392f474eea927324837a4fa1d708f637a99ac7cf
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-02-05 03:26:13 +0000

    premilinary

---
+++ app/controllers/square_controller.rb
@@ -0,0 +1,12 @@
+class SquareController < ApplicationController
+  def new_square
+    render({:template => "square"})
+  end
+
+  def compute_square
+    @square_input = params.fetch("user_square").to_f
+    @result = @square_input ** 2
+
+    render({:template => "square"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+  get("/", {:controller => "square", :action => "new_square"})
+  get("/square_root/new", {:controller => "sqrt", :action => "compute_sqrt"})
+  get("/payment/new", {:controller => "payment", :action => "compute_payment"})
+  get("/random/new", {:controller => "random", :action => "compute_random"})
 end
